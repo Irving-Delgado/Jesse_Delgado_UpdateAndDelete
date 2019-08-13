62469b2c4977c395134be39446a9698399802cea
Jesse-delgado <50339137+Jesse-delgado@users.noreply.github.com>
2019-08-13 14:07:23 -0500

    Add files via upload

---
+++ mvc/src/main/java/com/jesse/mvc/repositories/BookRepository.java
@@ -0,0 +1,22 @@
+package com.jesse.mvc.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jesse.mvc.models.Book;
+
+import java.util.List;
+
+@Repository 
+public interface BookRepository extends CrudRepository<Book, Long> {
+	// this method retrieves all the books from the database
+	List<Book> findAll();
+    // this method find a book by their description
+    List<Book> findByDescriptionContaining(String search);
+    // this method counts how many titles contain a certain string
+    Long countByTitleContaining(String search);
+    // this method deletes a book that starts with a specific title
+    Long deleteByTitleStartingWith(String search);
+   
+}
+
